c36221ba33fa2ced64a5026f7081bd8b2856bc98
ajaymarimuthu <ajayindia8001@gmail.com>
2024-11-30 16:36:01 +0530

    Javascript Array methods

---
+++ ArrayMethods/main.js
@@ -0,0 +1,61 @@
+const arr = [1, 2, 3, 1, 4, 5, 6];
+const arr2 = [1, 2, 3];
+// const res= [...arr, ...arr2]
+//  const res = arr.concat(arr2)
+// concat;inating arrays
+
+const strArr = ["Ajay", "vijay", "cj"];
+
+// const res = strArr.find(item => item === 'ra')
+//takes callback and find index
+
+// const res= arr.findIndex(item => item === 32);
+//takes callback and find index
+// if not return -1
+
+// const res = arr.indexOf(1, 4);
+//takes element itself and we can passs second argument start index find index
+// if not return -1
+
+// const res = arr.lastIndexOf(1, 4);
+//takes element itself and we can passs second argument start index find index
+// if not return -1
+
+// const res = arr.some((item) => item === 8);
+// checks atleast any one element satifies the condition
+
+// const res = arr.some((item) => item === 8);
+// if yes return true
+// if NO return false
+// checks atleast any one element satifies the condition
+
+// const res = arr.every((item) => item === 8);
+// if yes return true
+// if yes return false
+// checks every element satifies the condition
+
+// const res = arr.inclues((2);
+// if yes return true
+// if yes return false
+//takes element itself and we can pass fromIndex as second param
+
+// const res = arr.push(20);
+// modidifes old arr and add element at the end  and retusn new length
+
+// const res = arr.unshift(20);
+// modidifes old arr and add element at the begining  and retusn new length
+
+// const res = arr.pop(20);
+// modidifes old arr and remeove element at the end  and return the removed element
+
+// const res = arr.shift(1);
+// modidifes old arr and remove element at the begining  and retusn the element
+
+// const res = arr.join("");
+// return s string as cmma separated we can pass argumsts to separate based on that
+
+// const res = arr.reverse().toString();
+// returna string as reversed
+
+
+console.log(res);
